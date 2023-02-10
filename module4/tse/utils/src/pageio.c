fbed8d62eead84b73340346daaa6f16fe3cb3e75
MariaC27 <maria.h.cristoforo.24@dartmouth.edu>
2023-02-09 20:40:05 -0500

    half of step 1

--- module4/tse/utils/src/pageio.c
+++ module4/tse/utils/src/pageio.c
@@ -3,11 +3,17 @@
  * 
  * Author: Maria H. Cristoforo
  * Created: Thu Feb  9 19:59:42 2023 (-0500)
- * Version: 
+ * Version: 1.0
  * 
- * Description: pagesave and pageload functions
+ * Description: implementation of pageio.h: pagesave and pageload functions
  * 
  */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include "webpage.h"
 
 int32_t pagesave(webpage_t *pagep, int id, char *dirname){
 	
@@ -39,4 +45,28 @@ int32_t pagesave(webpage_t *pagep, int id, char *dirname){
 	free(ch1);
 	fclose(fp);
 	return 0;
+}
+
+webpage_t *pageload(int id, char *dirnm){
+	
+	//get a string of dirname and id
+	char new_id[10];
+	sprintf(new_id, "%d", id); //turns id into a string
+	char *ch1 = malloc(strlen(dirname)+2);
+	strcpy(ch1, dirname);
+	strcat(ch1, new_id); //concatenate dir and id together into ch1
+ 
+	//open file to read from
+	FILE *fp = fopen(ch1, "r");
+	if (fp == NULL) {
+		printf("Could not open file\n");
+		exit(EXIT_FAILURE);
+	}
+
+	fgets(url, 1000, fp); //unsure if these are right
+	fgets(depth, 5, fp);
+	webpage_t *newpage = webpage_new(url, depth, NULL);
+
+	fclose(fp);
+	return webpage_t;
 }
