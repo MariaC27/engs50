38eb41452f4758e27fbe4a6a033e9aa33481fe18
Justin Sapun <f006jnn@m210-linux-22.thayer.dartmouth.edu>
2023-02-14 13:18:30 -0500

    Step 3 Done

--- module5/tse/indexer/indexer.c
+++ module5/tse/indexer/indexer.c
@@ -22,25 +22,30 @@
 static int word_total;
 
 typedef struct wordcount{
-	char word_data[20];
+	char *word_data;
 	int count;
 }wordcount_t;
 
-
-static bool wordsearch(void *elementp, const void* searchkeyp){
-	wordcount_t *word_element = elementp;
-	if(!strcmp(searchkeyp, word_element->word_data))
+bool wordsearch(void* elementp, const void* searchkeyp){
+	wordcount_t* word_element = elementp;
+	if(!strcmp(word_element->word_data, searchkeyp)) 
 		return true;
 	return false;
 }
 
-void increase_wordcount(wordcount_t *word){
+void increase_wordcount(wordcount_t *word){ // function to increase word count per iteration
 	word->count++;
 }
 
-void increase_word_total(void *ep){
+void increase_word_total(void *ep){ // apply to all functions to get total count of all word iterations
 	wordcount_t *temp = ep;
 	word_total += temp->count;
+}
+
+void del_hash_word(void* data){ // this function tries to free up word_data pointer and tmp for every element
+	wordcount_t *tmp = data;
+	free(tmp->word_data);
+	free(tmp);
 }
 
 static int normalize_word(char *word){
@@ -57,54 +62,44 @@ static int normalize_word(char *word){
 }
 
 int main(void){
+	
 	hashtable_t *h1 = hopen(1000);
-
 	webpage_t* page1 = pageload(1, "../pages/");
+
+	char *path = "./testout.txt";
+	FILE *out;
+	if((out = fopen(path, "w")) == NULL){printf("Could not open file."); exit(EXIT_FAILURE);}
+
 	int pos = 0;
 	char *word;
-	char *path = "./testout.txt";
-	FILE *out = fopen(path, "w");
-	if(out == NULL){
-		printf("Could not open file\n");
-		exit(EXIT_FAILURE);
-	}
-
-
-	//wordcount_t *array[141];
-	//int counter = 0;
 	while((pos = webpage_getNextWord(page1, pos, &word)) > 0){
-		if(normalize_word(word)==0){
-			wordcount_t *found_word = hsearch(h1, wordsearch, word, strlen(word));
+
+		if(normalize_word(word)==0){ // Normalize word here
+			wordcount_t *found_word = hsearch(h1, wordsearch, (void *)word, strlen(word)); // hash search
 			fprintf(out, "%s\n", word); // print to file
+
 			if(found_word == NULL){
-				printf("%s\n", word);
-				wordcount_t *tmp = (wordcount_t *)malloc(sizeof(wordcount_t));
-				tmp->word_data = 				checkp(malloc(strlen(word)+1), "tmp->word_data");
-
-				//array[counter] = tmp;
+				wordcount_t *tmp = (wordcount_t *)malloc(sizeof(wordcount_t)); // malloc struct
+				tmp->word_data = malloc(strlen(word)+1);                       // malloc string
 				strcpy(tmp->word_data, word);
-
 				tmp->count = 1;
-				hput(h1, (void *)tmp, word, strlen(word));				
+				hput(h1, (void *)tmp, (void *)word, strlen(word));				
 			}
 			else{
-				found_word->count++;
+				found_word->count++; // if duplicate -->> increase word count
 			}
 	  }
-		free(word);
-		//counter++;
+		free(word); // free second pointer from webpage.c
 	}
 	
 	word_total = 0;
+	happly(h1, increase_word_total); // calculate sum of word iterations
+	printf("Sum word count after hash: %i\n", word_total);
 
-	//happly(h1, increase_word_total);
-	printf("\n\n%i\n\n", word_total);
+	happly(h1, del_hash_word);
 	webpage_delete(page1);
 	hclose(h1);
-	//for(int i = 0; i < 141; i++){
-	//free(array[i]);
-	//}
-	fclose(out);
+ 	fclose(out);
 	
 	exit(EXIT_SUCCESS);
 }
