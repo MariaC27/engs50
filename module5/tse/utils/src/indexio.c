e91c8b475d38c0b57929208ee723c92c537374da
MariaC27 <maria.h.cristoforo.24@dartmouth.edu>
2023-02-15 17:14:43 -0500

    step6 work

--- module5/tse/utils/src/indexio.c
+++ module5/tse/utils/src/indexio.c
@@ -13,13 +13,36 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include <hash.h>
 
 int main(void){
 	printf("Hello\n");
 }
 
-int indexsave(void){
+void get_word_data(void *w){
+	wordcount_t *theword = w;
+	// get doc and counts
 }
 
-int indexload(void){
+// take a hash table (index) as argument and write info to file
+int indexsave(hastable_t *h1){
+	// one line for each word in the index
+	// each line has word to start, then doc ID + count repeated on line
+	// name of file: indexnm
+	// return 0 or 1 based on success
+
+	FILE *fp = fopen("indexnm", "w");
+
+	if (fp == NULL){
+		printf("Could not open file\n");
+		return 1;
+  }
+
+	//use apply - for all words in hash,  write word and doc + counts to line
+}
+
+
+
+// load info from file and back into hash table (index)
+hastable_t* indexload(void){
 }
